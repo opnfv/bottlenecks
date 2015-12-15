c3f9ac49b773679e9f929f542619140bf3c6ef49
QiLiang <liangqi1@huawei.com>
2015-12-15 01:30:54 +0000

    Update Benchmark configure
    JIRA:BOTTLENECK-24
    
    Change-Id: I1bf8c3fd555da1ddf8919838c5d20c9f794c0d2e
    Signed-off-by: QiLiang <liangqi1@huawei.com>

---
+++ rubbos/rubbos_scripts/1-1-1/scripts/BENCHMARK_pkg_prepare.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+. ../set_bottlenecks_rubbos_env.sh
+
+scp_options="-o StrictHostKeyChecking=no -o BatchMode=yes"
+
+# Packages for BENCHMARK rubbos install
+if true; then
+ssh $BENCHMARK_HOST "mkdir -p /bottlenecks/rubbos/rubbos_scripts/1-1-1"
+scp $scp_options ../set_bottlenecks_rubbos_env.sh $BENCHMARK_HOST:/bottlenecks/rubbos/rubbos_scripts/1-1-1
+
+ssh $BENCHMARK_HOST "
+    apt-get update
+    apt-get install -y \
+        gcc \
+        g++ \
+        make
+"
+
+ssh $BENCHMARK_HOST "mkdir -p $SOFTWARE_HOME"
+scp $scp_options $SOFTWARE_HOME/$RUBBOS_TARBALL $BENCHMARK_HOST:$SOFTWARE_HOME/$RUBBOS_TARBALL
+scp $scp_options $SOFTWARE_HOME/flush_cache $BENCHMARK_HOST:$SOFTWARE_HOME/flush_cache
+scp $scp_options $SOFTWARE_HOME/$SYSSTAT_TARBALL $BENCHMARK_HOST:$SOFTWARE_HOME/$SYSSTAT_TARBALL
+ssh $BENCHMARK_HOST "mkdir -p $OUTPUT_HOME/rubbos_conf"
+scp $scp_options $OUTPUT_HOME/rubbos_conf/cpu_mem.sh $BENCHMARK_HOST:$OUTPUT_HOME/rubbos_conf/cpu_mem.sh
+fi
+
+# Packages for BENCHMARK install
+if true; then
+scp $scp_options $SOFTWARE_HOME/$JAVA_TARBALL $BENCHMARK_HOST:$SOFTWARE_HOME/$JAVA_TARBALL
+fi
+
+# Packages for BENCHMARK configure
+if true; then
+ssh $BENCHMARK_HOST "mkdir -p $WORK_HOME/rubbos_files"
+scp $scp_options -r $WORK_HOME/rubbos_files/Client $BENCHMARK_HOST:$WORK_HOME/rubbos_files
+scp $scp_options -r $WORK_HOME/rubbos_files/bench $BENCHMARK_HOST:$WORK_HOME/rubbos_files
+ssh $BENCHMARK_HOST "mkdir -p $OUTPUT_HOME/rubbos_conf"
+for i in build.properties config.mk Makefile \
+         rubbos-servletsBO.sh rubbos-servletsRW.sh
+do
+    scp $scp_options -r $OUTPUT_HOME/rubbos_conf/$i $BENCHMARK_HOST:$OUTPUT_HOME/rubbos_conf/$i
+done
+fi
+
--- rubbos/rubbos_scripts/1-1-1/scripts/run.sh
+++ rubbos/rubbos_scripts/1-1-1/scripts/run.sh
@@ -7,6 +7,15 @@ echo "*** scp scripts *************************************************"
 scp_options="-o StrictHostKeyChecking=no -o BatchMode=yes"
 
 if true; then
+for script in BENCHMARK_rubbos_install.sh BENCHMARK_install.sh \
+              BENCHMARK_configure.sh BENCHMARK_uninstall.sh \
+              BENCHMARK_rubbos_uninstall.sh
+do
+    scp $scp_options $script $BENCHMARK_HOST:/tmp
+done
+fi
+
+if true; then
 for script in HTTPD_install.sh HTTPD_rubbos_install.sh \
               HTTPD_configure.sh HTTPD_ignition.sh \
               HTTPD_stop.sh HTTPD_rubbos_uninstall.sh \
@@ -35,13 +44,14 @@ for script in MYSQL1_install.sh MYSQL1_rubbos_install.sh \
 do
     scp $scp_options $script $MYSQL1_HOST:/tmp
 done
+fi
 
 # Prepare software packages
 echo "*** prepare software packages ***"
 ./MYSQL1_pkg_prepare.sh
 ./TOMCAT1_pkg_prepare.sh
 ./HTTPD_pkg_prepare.sh
-
+./BENCHMARK_pkg_prepare.sh
 
 # Install and Configure and run Apache, Tomcat, CJDBC, and MySQL
 echo "*** install scripts & configure & execute ***********************"
@@ -64,6 +74,12 @@ ssh $TOMCAT1_HOST /tmp/TOMCAT1_rubbos_install.sh
 ssh root@$HTTPD_HOST chmod 770 /tmp/HTTPD_rubbos_install.sh
 ssh $HTTPD_HOST /tmp/HTTPD_rubbos_install.sh
 
+ssh root@$BENCHMARK_HOST chmod 770 /tmp/BENCHMARK_rubbos_install.sh
+ssh $BENCHMARK_HOST /tmp/BENCHMARK_rubbos_install.sh
+
+ssh root@$BENCHMARK_HOST chmod 770 /tmp/BENCHMARK_install.sh
+ssh $BENCHMARK_HOST /tmp/BENCHMARK_install.sh
+
 ssh root@$MYSQL1_HOST chmod 770 /tmp/MYSQL1_configure.sh
 ssh $MYSQL1_HOST /tmp/MYSQL1_configure.sh
 
@@ -72,6 +88,9 @@ ssh $TOMCAT1_HOST /tmp/TOMCAT1_configure.sh
 
 ssh root@$HTTPD_HOST chmod 770 /tmp/HTTPD_configure.sh
 ssh $HTTPD_HOST /tmp/HTTPD_configure.sh
+
+ssh root@$BENCHMARK_HOST chmod 770 /tmp/BENCHMARK_configure.sh
+ssh $BENCHMARK_HOST /tmp/BENCHMARK_configure.sh
 
 ssh root@$TOMCAT1_HOST chmod 770 /tmp/TOMCAT1_rubbosSL_configure.sh
 ssh $TOMCAT1_HOST /tmp/TOMCAT1_rubbosSL_configure.sh
