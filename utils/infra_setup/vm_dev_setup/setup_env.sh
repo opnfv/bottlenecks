7487c5fea13ebb678d7a1b8931a160347b23a4ac
QiLiang <liangqi1@huawei.com>
2016-01-08 01:27:55 +0000

    test
    Change-Id: Ie83fcf0e4a4ad2da715a694118f1f9e916a5f704
    Signed-off-by: QiLiang <liangqi1@huawei.com>

--- utils/infra_setup/vm_dev_setup/setup_env.sh
+++ utils/infra_setup/vm_dev_setup/setup_env.sh
@@ -66,6 +66,7 @@ bottlenecks_download_repo()
         sudo rm -rf $BOTTLENECKS_REPO_DIR
         sudo git clone $BOTTLENECKS_REPO $BOTTLENECKS_REPO_DIR
         if [ x"$GERRIT_REFSPEC_DEBUG" != x ]; then
+            echo "fetch $GERRIT_REFSPEC_DEBUG"
             sudo git fetch $BOTTLENECKS_REPO $GERRIT_REFSPEC_DEBUG && git checkout FETCH_HEAD
         fi
 
@@ -116,6 +117,7 @@ main()
     source $SCRIPT_DIR/common.sh
 
     bottlenecks_prepare_env
+    set -x
     bottlenecks_download_repo
     bottlenecks_config_hosts_ip
     bottlenecks_download_packages
