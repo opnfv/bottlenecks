ec17aec8db4be8d56df181cb7cc0875ac8833727
QiLiang <liangqi1@huawei.com>
2016-01-16 01:14:18 +0000

    Add Dockerfile and bootstrap scripts to run rubbos
    JIRA: -
    
    Change-Id: Ifc14193ee2d6f0a1f9f5b67a2043ed6640a8b928
    Signed-off-by: QiLiang <liangqi1@huawei.com>

--- ci/run.sh
+++ ci/run.sh
@@ -5,13 +5,14 @@ set -ex
 BOTTLENECKS_REPO=https://gerrit.opnfv.org/gerrit/bottlenecks
 
 SCRIPT_DIR=`cd ${BASH_SOURCE[0]%/*};pwd`
-GERRIT_REFSPEC_DEBUG=$1
+export BOTTLENECKS_TOP_DIR=$SCRIPT_DIR/../
+export GERRIT_REFSPEC_DEBUG=$1
 
 if [ x"$GERRIT_REFSPEC_DEBUG" != x ]; then
     git fetch $BOTTLENECKS_REPO $GERRIT_REFSPEC_DEBUG && git checkout FETCH_HEAD
 fi
 
-$SCRIPT_DIR/../utils/infra_setup/heat_template/HOT_create_instance.sh $GERRIT_REFSPEC_DEBUG
+$SCRIPT_DIR/rubbos_docker_run.sh
 
 set +ex
 
