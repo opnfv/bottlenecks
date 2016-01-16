33ee15d4077061bb660da802b00cca44b9d33386
QiLiang <liangqi1@huawei.com>
2016-01-16 09:58:05 +0000

    Scripts update to run rubbos on compass4nfv env 2
    JIRA: -
    
    Change-Id: Ibe16adc4a6a1810a75b60ef9198ce7ea239d79c3
    Signed-off-by: QiLiang <liangqi1@huawei.com>

--- ci/rubbos_docker_run.sh
+++ ci/rubbos_docker_run.sh
@@ -5,12 +5,12 @@ set -ex
 docker build -t opnfv/bottlenecks ${BOTTLENECKS_TOP_DIR}/ci/docker/bottlenecks-ci/
 
 if [ x"${GERRIT_REFSPEC_DEBUG}" != x ]; then
-    opts="--privileged=true -it"
+    opts="--privileged=true"
 else
     opts="--privileged=true --rm"
 fi
 
-envs="-e INSTALLER_TYPE=${INSTALLER_TYPE} -e INSTALLER_IP=${INSTALLER_IP} -e NODE_NAME=${NODE_NAME} -e EXTERNAL_NETWORK=${EXTERNAL_NETWORK} -e BOTTLENECKS_BRANCH=${GIT_BRANCH##origin/} -e GERRIT_REFSPEC_DEBUG=${GERRIT_REFSPEC_DEBUG} -e BOTTLENECKS_DB_TARGET=${BOTTLENECKS_DB_TARGET} -e PACKAGE_URL=${PACKAGE_URL} -e EXTERNAL_NET=${EXTERNAL_NET}"
+envs="-e INSTALLER_TYPE=${INSTALLER_TYPE} -e INSTALLER_IP=${INSTALLER_IP} -e NODE_NAME=${NODE_NAME} -e EXTERNAL_NET=${EXTERNAL_NETWORK} -e BOTTLENECKS_BRANCH=${GIT_BRANCH##origin/} -e GERRIT_REFSPEC_DEBUG=${GERRIT_REFSPEC_DEBUG} -e BOTTLENECKS_DB_TARGET=${BOTTLENECKS_DB_TARGET} -e PACKAGE_URL=${PACKAGE_URL}"
 volumes="-v ${BOTTLENECKS_TOP_DIR}:${BOTTLENECKS_TOP_DIR}"
 create_instance=${BOTTLENECKS_TOP_DIR}/utils/infra_setup/heat_template/HOT_create_instance.sh
 
