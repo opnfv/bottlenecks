9185ecedc1473efd946f8f29525c7dbdf75744a4
MatthewLi <matthew.lijun@huawei.com>
2016-02-04 02:18:29 -0500

    delete file header
    Change-Id: I4450e007122fa90ef3326b0a64f6195ae14046dc
    Signed-off-by: MatthewLi <matthew.lijun@huawei.com>

--- rubbos/rubbos_scripts/1-1-1/rubbos_conf/Config.java
+++ rubbos/rubbos_scripts/1-1-1/rubbos_conf/Config.java
@@ -1,38 +1,4 @@
-
-/**
- * RUBBoS: Rice University Bulletin Board System.
- * Copyright (C) 2001-2004 Rice University and French National Institute For 
- * Research In Computer Science And Control (INRIA).
- * Contact: jmob@objectweb.org
- * 
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or any later
- * version.
- * 
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- *
- * Initial developer(s): Emmanuel Cecchet.
- * Contributor(s): ______________________.
- */
-
 package edu.rice.rubbos.servlets;
-
-/**
- * This class contains the configuration for the servlets like the path of HTML
- * files, etc ...
- * 
- * @author <a href="mailto:cecchet@rice.edu">Emmanuel Cecchet </a> and <a
- *         href="mailto:julie.marguerite@inrialpes.fr">Julie Marguerite </a>
- * @version 1.0
- */
 
 public class Config
 {
