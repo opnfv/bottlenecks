088a64fec7b760601a825d3081dff9ebd85428d5
MatthewLi <matthew.lijun@huawei.com>
2016-03-24 05:19:16 -0400

    add united test suite entrance
    Change-Id: Ifd2fca716d45ade42c679048ce29eb1e157e7e59
    JIRA: BOTTLENECK-45

---
+++ ci/common.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+mkdir -p /home/opnfv/bottlenecks/config
+config_file=/home/opnfv/functest/config/bottlenecks_cfg.yaml
+
+if [ ! -f ${config_file} ]; then
+    default_config_file=$(find /home/opnfv/repos -name bottlenecks_cfg.yaml)
+    cp $default_config_file $config_file
+    echo "bottlenecks_cfg.yaml not provided. Using default one"
+fi
+
+SUITE_PREFIX_CONFIG=$(cat $config_file | grep -w suite_prefix_config | awk 'END {print $NF}')
+
+info () {
+    logger -s -t "bottlenecks.info" "$*"
+}
+
+error () {
+    logger -s -t "bottlenecks.error" "$*"
+    exit 1
+}
