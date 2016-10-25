b60b5daa668899d528c70c1d4858421eb71e73a6
liyin <liyin11@huawei.com>
2016-10-25 00:39:22 -0400

    Modify POSCA code into PEP8 style
    JIRA: BOTTLENECK-102
    
    Modify all the code in the testcase POSCA into PEP8 style.
    Using more standard way to realize our function.
    
    Change-Id: Ic014678db454b8a36b36395c32b4769f6cf700be
    Signed-off-by: liyin <liyin11@huawei.com>

--- testsuites/posca/run_posca.py
+++ testsuites/posca/run_posca.py
@@ -8,32 +8,36 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-import os
 import sys
 import subprocess
 
 INTERPRETER = "/usr/bin/python"
-#------------------------------------------------------
+# ------------------------------------------------------
 # run posca testcase
 # ------------------------------------------------------
+
+
 def posca_run(arg):
-    print "========== run posca ==========="
+    print("========== run posca ==========")
     if(arg == "factor_system_bandwidth"):
-        print "========== run posca_system_bandwidth ==========="
-        cmd = '/home/opnfv/bottlenecks/testsuites/posca/testcase_script/posca_factor_system_bandwidth.py'
-        pargs = [INTERPRETER,cmd]
+        print("========== run posca_system_bandwidth ===========")
+        cmd = '/home/opnfv/bottlenecks/testsuites/posca/testcase_script/\
+                posca_factor_system_bandwidth.py'
+        pargs = [INTERPRETER, cmd]
         sub_result = subprocess.Popen(pargs)
         sub_result.wait()
 
+
 def posca_env_check():
-    print "========== posca env check ==========="
+    print("========== posca env check ===========")
+
 
 def main():
-    para_testname = sys.argv[0]
+    # para_testname = sys.argv[0]
     para_test_arg = sys.argv[1]
     posca_env_check()
     posca_run(para_test_arg)
     sys.exit(0)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
