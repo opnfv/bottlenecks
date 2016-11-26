94551f399635ea51a08db8ddaa3ae8dca374a84b
yuyang <Gabriel.yuyang@huawei.com>
2016-11-26 23:07:41 +0800

    Add CLI framework for Bottlenecks
    JIRA: BOTTLENECK-65
    
    Adding cli framwork for Bottlenecks.
    
    Usage:
    bottlenecks testcase run <testcase>
    
    Currently only the cli framwork and testcase command group is added.
    run, list, etc., commands and openstack command group
    will added afterwards.
    
    Two setup entries are added. One is within the top directory and the
    other is within the cli directory.
    
    Change-Id: I304f6f2e9b29b9f582953c2739806c1fb3e87fed
    Signed-off-by: yuyang <Gabriel.yuyang@huawei.com>

---
+++ cli/bottlenecks_cli.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+##############################################################################
+# Copyright (c) 2016 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import click
+
+from command_group.testcase import Testcase
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version='0.1')
+@click.pass_context
+def main(ctx):
+    '''cli for bottlenecks project
+
+       commands:
+       bottlenecks testcase run <testcase>
+    '''
+    pass
+
+_testcase = Testcase()
+
+@main.group()
+@click.pass_context
+def testcase(ctx):
+    '''testcase cli group for bottlenecks project'''
+    pass
+
+@testcase.command('run', help="To execute a test case.")
+@click.argument('testname', type=click.STRING, required=True)
+@click.option('-n', '--noclean', is_flag=True, default=False,
+              help='Openstack resources created by the test'
+              'will not be cleaned after the testcase execution.')
+def testcase_run(testname, noclean):
+    _testcase.run(testname, noclean)
