61760324d8a03a26028c487516a5e47000952d3e
yuyang <Gabriel.yuyang@huawei.com>
2016-11-26 23:07:41 +0800

    Add CLI framework for Bottlenecks
    JIRA: BOTTLENECK-65
    
    Adding cli framwork for Bottlenecks.
    
    Usage:
    bottlenecks testcase run <testcase>
    
    Currently only the cli framwork and testcase command group is added.
    run, list, etc., commands and openstack command group
    will added afterwards.
    
    Two setup entries are added. One is within the top directory and the
    other is within the cli directory.
    
    Change-Id: I304f6f2e9b29b9f582953c2739806c1fb3e87fed
    Signed-off-by: yuyang <Gabriel.yuyang@huawei.com>

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+##############################################################################
+# Copyright (c) 2016 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="bottlenecks",
+    version="master",
+    py_modules=['bottlenecks_cli'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+    },
+    url="https://www.opnfv.org",
+    install_requires=["click"],
+    entry_points={
+        'console_scripts': [
+            'bottlenecks=cli.bottlenecks_cli:main'
+        ],
+    },
+)
