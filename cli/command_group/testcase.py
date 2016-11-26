7aaf17f0cfbcfcf12cb2e0424d9f3603b11431c3
yuyang <Gabriel.yuyang@huawei.com>
2016-11-26 23:07:41 +0800

    Add CLI framework for Bottlenecks
    JIRA: BOTTLENECK-65
    
    Adding cli framwork for Bottlenecks.
    
    Usage:
    bottlenecks testcase run <testcase>
    
    Currently only the cli framwork and testcase command group is added.
    run, list, etc., commands and openstack command group
    will added afterwards.
    
    Two setup entries are added. One is within the top directory and the
    other is within the cli directory.
    
    Change-Id: I304f6f2e9b29b9f582953c2739806c1fb3e87fed
    Signed-off-by: yuyang <Gabriel.yuyang@huawei.com>

---
+++ cli/command_group/testcase.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+##############################################################################
+# Copyright (c) 2016 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import os
+
+
+class Testcase(object):
+    """Test command group.
+
+       Set of commnads to execute and list testcases
+    """
+
+    def __init__(self):
+        self.test_case_path = '/home/opnfv/bottlenecks/testsuites/'
+        self.test_case_list = []
+
+    def run(self, testname, noclean=False):
+        if testname == 'posca':
+            os.system('bash /home/opnfv/bottlenecks/run_tests.sh -s posca')
