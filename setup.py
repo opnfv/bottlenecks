53b9c2d42caa9ac16d888377b2ebe70beb33392b
liyin <liyin11@huawei.com>
2016-12-28 14:25:06 +0800

    Bottlenecks frame support stack create
    JIRA: BOTTLENECK-120
    
    add the common.py,manager.py,template.py,
    could use the function
    import utils.infra_setup.heat.x
    then the code could import the package.
    
    Change-Id: I7bbd115bd13d87c4f43b7e4bb35e6a6bf8465bc8
    Signed-off-by: liyin <liyin11@huawei.com>

--- setup.py
+++ setup.py
@@ -18,6 +18,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
+        'utils': [
+            'utils/infra_setup/heat/*.py'
+        ]
     },
     url="https://www.opnfv.org",
     install_requires=["click"],
