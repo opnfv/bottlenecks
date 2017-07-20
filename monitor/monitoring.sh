3b9914a6712047f4e55dd6765b3a88f75cab59b9
yuyang <Gabriel.yuyang@huawei.com>
2017-07-20 13:07:25 +0800

    Add Grafana for displaying time series from Prometheus
    JIRA: BOTTLENECK-172
    
    Add Grafana for the monitoring result dashboarding
    
    Change-Id: I9b17f4bd745c8556e2b551c6b499d77e3406cbf4
    Signed-off-by: yuyang <Gabriel.yuyang@huawei.com>

--- monitor/monitoring.sh
+++ monitor/monitoring.sh
@@ -9,9 +9,11 @@
 ##############################################################################
 
 MONITOR_CONFIG="/home/opnfv/bottlenecks/monitor/config"
+GRAFANA="/home/opnfv/bottlenecks/monitor/grafana"
 
 # Node-Exporter
-sudo docker run -d -p 9100:9100 \
+sudo docker run --name Bottlenecks-Node-Exporter \
+  -d -p 9100:9100 \
   -v "/proc:/host/proc:ro" \
   -v "/sys:/host/sys:ro" \
   -v "/:/rootfs:ro" \
@@ -22,18 +24,24 @@ sudo docker run -d -p 9100:9100 \
     -collector.filesystem.ignored-mount-points "^/(sys|proc|dev|host|etc)($|/)"
 
 # Collectd
-sudo docker run -d \
+sudo docker run --name Bottlenecks-Collectd -d \
   --privileged \
   -v ${MONITOR_CONFIG}:/etc/collectd:ro \
   -v /proc:/mnt/proc:ro \
   fr3nd/collectd
 
 # Collectd-Exporter
-sudo docker run -d -p 9103:9103 \
+sudo docker run --name Bottlenecks-Collectd-Exporter \
+  -d -p 9103:9103 \
   -p 25826:25826/udp prom/collectd-exporter \
   -collectd.listen-address=":25826"
 
 # Prometheus
-sudo docker run -d -p 9090:9090 \
-  -v ${MONITOR_CONFIG}/prometheus.yaml:/etc/prometheus/prometheus.yaml \
+sudo docker run --name Bottlenecks-Prometheus \
+  -d -p 9090:9090 \
+  -v ${MONITOR_CONFIG}/prometheus.yaml:/etc/prometheus/prometheus.yml \
   prom/prometheus
+
+sudo  docker run --name Bottlenecks-Grafana \
+  -d -p 3000:3000 \
+  grafana/grafana
