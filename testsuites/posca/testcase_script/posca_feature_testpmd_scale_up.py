9ebfae66e79830042dc9842be86394ff2df660a9
liyin <liyin11@huawei.com>
2017-09-20 09:06:34 +0800

    draft:testpmd scale-up testcase.
    JIRA: BOTTLENECK-205
    
    This is the draft of testpmd scale-up feature testcase.
    
    Change-Id: I4a6de0e7e1ea7c04639ece78a7b11b75128d8d2f
    Signed-off-by: liyin <liyin11@huawei.com>

--- testsuites/posca/testcase_script/posca_feature_testpmd_scale_up.py
+++ testsuites/posca/testcase_script/posca_feature_testpmd_scale_up.py
@@ -21,22 +21,17 @@ from utils.parser import Parser as conf_parser
 import utils.env_prepare.stack_prepare as stack_prepare
 import testsuites.posca.testcase_dashboard.system_bandwidth as DashBoard
 import utils.infra_setup.runner.docker_env as docker_env
+import utils.infra_setup.runner.yardstick as yardstick_task
+
 # --------------------------------------------------
 # logging configuration
 # --------------------------------------------------
 LOG = log.Logger(__name__).getLogger()
 
-test_dict = {
-    "action": "runTestCase",
-    "args": {
-        "opts": {
-            "task-args": {}
-        },
-        "testcase": "netperf_bottlenecks"
-    }
-}
 testfile = os.path.basename(__file__)
 testcase, file_format = os.path.splitext(testfile)
+cidr = "/home/opnfv/repos/yardstick/tests/vsperf/pvp_rfc2544_throughput_dpdk.yaml"
+runner_DEBUG = False
 
 
 def env_pre(con_dic):
@@ -46,23 +41,23 @@ def env_pre(con_dic):
 
 def config_to_result(test_config, test_result):
     testdata = {}
-    parser_result = test_result["benchmark"]["data"]
-    test_result.update(test_config)
-    test_result.update(parser_result)
-    test_result["throughput"] = float(test_result["throughput"])
-    test_result["remote_cpu_util"] = float(test_result["remote_cpu_util"])
-    test_result["local_cpu_util"] = float(test_result["local_cpu_util"])
-    test_result["mean_latency"] = float(test_result["mean_latency"])
-    testdata["data_body"] = test_result
-    testdata["testcase"] = testcase
+    result = []
+    test_result = result["result"]["testcase"]["pvp_rfc2544_throughput_dpdk"]["tc_data"]
+    for result in len(test_result):
+        testdata["vcpu"] = test_config["vcpu"]
+        testdata["memory"] = test_config["memory"]
+        testdata["nrFlows"] = test_result["data"]["nrFlows"]
+        testdata["packet_size"] = test_result["data"]["packet_size"]
+        testdata["throughput"] = test_result["data"]["throughput_rx_mbps"]
+        result.append(testdata)
     return testdata
 
 
 def testcase_parser(out_file="yardstick.out", **parameter_info):
-    cmd = ('yardstick task start /home/opnfv/repos/yardstick/'
-           'samples/netperf_bottlenecks.yaml --output-file ' + out_file)
-    cmd = cmd + " --task-args " + '"' + str(parameter_info) + '"'
-    LOG.info("yardstick test cmd is: %s" % cmd)
+    cmd = yardstick_task.yardstick_command_parser(debug=runner_DEBUG,
+                                                  cidr=cidr,
+                                                  outfile=out_file,
+                                                  parameter=parameter_info)
     return cmd
 
 
@@ -85,19 +80,22 @@ def do_test(test_config, Use_Dashboard, context_conf):
                 LOG.error("yardstick error exit")
                 exit()
 
-    save_data = config_to_result(test_config, data['result'][1])
+    save_data = config_to_result(test_config, data['result'])
     if Use_Dashboard is True:
         DashBoard.dashboard_send_data(context_conf, save_data)
 
-    return save_data["data_body"]
+    # return save_data["data_body"]
+    return save_data
 
 
 def run(test_config):
-    con_dic = test_config["load_manager"]
+    load_config = test_config["load_manager"]
+    scenarios_conf = load_config["scenarios"]
     Use_Dashboard = False
+
     env_pre(None)
     if test_config["contexts"]["yardstick_ip"] is None:
-        con_dic["contexts"]["yardstick_ip"] =\
+        load_config["contexts"]["yardstick_ip"] =\
             conf_parser.ip_parser("yardstick_test_ip")
 
     if "dashboard" in test_config["contexts"].keys():
@@ -108,49 +106,29 @@ def run(test_config):
         Use_Dashboard = True
         DashBoard.dashboard_system_bandwidth(test_config["contexts"])
 
-    data = {}
-    rx_pkt_a = con_dic['scenarios']['rx_pkt_sizes'].split(',')
-    tx_pkt_a = con_dic['scenarios']['tx_pkt_sizes'].split(',')
-    data["rx_pkt_sizes"] = rx_pkt_a
-    data["tx_pkt_sizes"] = tx_pkt_a
-    con_dic["result_file"] = os.path.dirname(
+    cpus = conf_parser.str_to_list(scenarios_conf["cpus"])
+    mems = conf_parser.str_to_list(scenarios_conf["mems"])
+    pkt_size = conf_parser.str_to_list(scenarios_conf["pkt_size"])
+    multistream = conf_parser.str_to_list(scenarios_conf["multistream"])
+    search_interval = scenarios_conf["search_interval"]
+
+    load_config["result_file"] = os.path.dirname(
         os.path.abspath(__file__)) + "/test_case/result"
-    cur_role_result = 1
-    pre_role_result = 1
-    pre_reply = {}
-    data_return = {}
-    data_max = {}
-    data_return["throughput"] = 1
 
-    for test_x in data["tx_pkt_sizes"]:
-        data_max["throughput"] = 1
-        bandwidth_tmp = 1
-        for test_y in data["rx_pkt_sizes"]:
-            case_config = {
-                "tx_msg_size": float(test_x),
-                "rx_msg_size": float(test_y),
-                "test_time": con_dic['scenarios']['test_times'],
-                "pod_info": conf_parser.bottlenecks_config["pod_info"]
-            }
-            data_reply = do_test(case_config, Use_Dashboard,
-                                 test_config["contexts"])
+    if len(cpus) != len(mems):
+        LOG.error("the cpus and mems config data number is not same!")
+        os._exit()
 
-            conf_parser.result_to_file(data_reply, test_config["out_file"])
-            bandwidth = data_reply["throughput"]
-            if (data_max["throughput"] < bandwidth):
-                data_max = data_reply
-            if (abs(bandwidth_tmp - bandwidth) / bandwidth_tmp < 0.025):
-                LOG.info("this group of data has reached top output")
-                break
-            else:
-                pre_reply = data_reply
-                bandwidth_tmp = bandwidth
-        cur_role_result = float(pre_reply["throughput"])
-        if (abs(pre_role_result - cur_role_result) / pre_role_result < 0.025):
-            LOG.info("The performance increases slowly")
-        if data_return["throughput"] < data_max["throughput"]:
-            data_return = data_max
-        pre_role_result = cur_role_result
+    for i in range(0, len(cpus)):
+        case_config = {"vcpu": cpus[i],
+                       "memory": mems[i],
+                       "multistream": multistream,
+                       "pktsize": pkt_size,
+                       "search_interval": search_interval}
+
+        data_reply = do_test(case_config, Use_Dashboard,
+                             test_config["contexts"])
+
     LOG.info("Find bottlenecks of this config")
-    LOG.info("The max data is %d", data_return["throughput"])
-    return data_return
+    LOG.info("The max data is %d", data_reply["throughput"])
+    return data_reply
