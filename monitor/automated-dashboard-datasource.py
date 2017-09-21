e6774d5049b901b9160b3999a1c8b8a0502c2ffe
Rutuja Surve <rutuja.r.surve@gmail.com>
2017-09-21 13:35:34 -0400

    JIRA: BOTTLENECK-180 Automated creation of Prometheus dashboard and datasource in Grafana
    Change-Id: Id1e0323f74a0587b0fe3c80ba7a39843893f00ec
    Signed-off-by: Rutuja Surve <rutuja.r.surve@gmail.com>

---
+++ monitor/automated-dashboard-datasource.py
@@ -0,0 +1,60 @@
+import logging
+import socket
+import requests
+from oslo_serialization import jsonutils
+
+
+logger = logging.getLogger(__name__)
+
+
+def _create_dashboard(ip, port, path):
+    url = 'http://admin:admin@{}:{}/api/dashboards/db'.format(ip, port)
+    logger.info("Fetched IP for dashboard creation!")
+    with open(path) as f:
+        data = jsonutils.load(f)
+    try:
+        post(url, {"dashboard": data})
+        logger.info( "Trying to post dashboard json!")
+    except Exception:
+        logger.info("Create dashboard failed")
+        raise
+
+
+def _create_data_source(ip, port):
+    url = 'http://admin:admin@{}:{}/api/datasources'.format(ip, port)
+    logger.info("Fetched URL for datasource")
+    data = {
+        "name": "automated-ds",
+        "type": "prometheus",
+        "access": "direct",
+        "url": "http://{}:9090".format(ip),
+    }
+    try:
+        post(url, data)
+        logger.info("Trying to post datasource")
+
+    except Exception:
+        logger.info("Create Datasources failed")
+        raise
+
+
+def post(url, data):
+    data = jsonutils.dump_as_bytes(data)
+    logger.info("In post method for dumping data")
+    headers = {'Content-Type': 'application/json'}
+    try:
+        response = requests.post(url, data=data, headers=headers)
+        result = response.json()
+        logger.debug('The result is: %s', result)
+        logger.info("Trying to post")
+        return result
+    except Exception as e:
+        logger.info("Failed post" + str(e))
+        raise
+
+
+ip_address = socket.gethostbyname(socket.gethostname())
+_create_dashboard(ip_address, 3000, '/var/lib/grafana/' +
+                  'dashboards/' +
+                  'prometheus-system_rev1.json')
+_create_data_source(ip_address, 3000)
