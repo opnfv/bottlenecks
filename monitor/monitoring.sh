6d003ff35584da49879770a9fbce8f3f9de8c314
Rutuja Surve <rutuja.r.surve@gmail.com>
2017-11-03 10:16:23 +0000

    Collectd Client-Server installation automation script
    JIRA: BOTTLENECK-181
    
    Automated the installation for collectd client from
    jump server host and corrected docker versions in monitoring
    script
    
    Change-Id: I14ec20f2761c72e261aa1185a381f0c02b84ed45
    Signed-off-by: Rutuja Surve <rutuja.r.surve@gmail.com>

--- monitor/monitoring.sh
+++ monitor/monitoring.sh
@@ -18,7 +18,7 @@ sudo docker run --name bottlenecks-node-exporter \
   -v "/sys:/host/sys:ro" \
   -v "/:/rootfs:ro" \
   --net="host" \
-  quay.io/prometheus/node-exporter \
+  quay.io/prometheus/node-exporter:v0.14.0 \
     -collector.procfs /host/proc \
     -collector.sysfs /host/sys \
     -collector.filesystem.ignored-mount-points "^/(sys|proc|dev|host|etc)($|/)"
@@ -28,22 +28,23 @@ sudo docker run --name bottlenecks-collectd -d \
   --privileged \
   -v ${MONITOR_CONFIG}:/etc/collectd:ro \
   -v /proc:/mnt/proc:ro \
-  fr3nd/collectd
+  fr3nd/collectd:5.5.0-1
 
 # Collectd-Exporter
 sudo docker run --name bottlenecks-collectd-exporter \
   -d -p 9103:9103 \
-  -p 25826:25826/udp prom/collectd-exporter \
+  -p 25826:25826/udp prom/collectd-exporter:0.3.1 \
   -collectd.listen-address=":25826"
 
 # Prometheus
 sudo docker run --name bottlenecks-prometheus \
   -d -p 9090:9090 \
   -v ${MONITOR_CONFIG}/prometheus.yaml:/etc/prometheus/prometheus.yml \
-  prom/prometheus
+  prom/prometheus:v1.7.1
 
 # Grafana
 sudo  docker run --name bottlenecks-grafana \
   -d -p 3000:3000 \
   -v ${GRAFANA}/config/grafana.ini:/etc/grafana/grafana.ini \
-  grafana/grafana
+  grafana/grafana:4.5.0
+
