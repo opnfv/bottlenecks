798c577d6b5ab09b7e930f9dccd1fdf696a499a0
Rutuja Surve <rutuja.r.surve@gmail.com>
2017-11-14 04:36:23 +0000

    Cadvisor Client-Server installation automation script
    JIRA: BOTTLENECK-181
    
    Automated the installation for cadvisor client from
    jump server host and added docker installation of cadvisor
    in monitoring script.
    
    Change-Id: I906b5237bc0ed320106309c1a29e8dd72a05a427
    Signed-off-by: Rutuja Surve <rutuja.r.surve@gmail.com>

---
+++ monitor/cadvisor_install.sh
@@ -0,0 +1,10 @@
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:rw \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --volume=/dev/disk/:/dev/disk:ro \
+  --publish=8080:8080 \
+  --detach=true \
+  --name=cadvisor \
+  google/cadvisor:v0.25.0 \ -storage_driver=Prometheus
