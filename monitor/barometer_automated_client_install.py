dddfbcb9e6592c7880319300d706fb59ec295f8c
Rutuja Surve <rutuja.r.surve@gmail.com>
2018-02-21 13:17:53 +0000

    Barometer automated installation script for jump server and compute/controller nodes
    JIRA: BOTTLENECK-182
    
    Install barometer for collecting OVS and collectd statistics as
    docker containers on all nodes
    
    Change-Id: I8ae7e2bcbe95ab6e1224e08828c822d50367f616
    Signed-off-by: Rutuja Surve <rutuja.r.surve@gmail.com>

---
+++ monitor/barometer_automated_client_install.py
@@ -0,0 +1,36 @@
+##############################################################################
+# Copyright (c) 2018 Huawei Tech and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import logging
+import sys
+import yaml
+sys.path.insert(0, '/home/opnfv/bottlenecks/utils/infra_setup/passwordless_SSH/')
+import ssh
+
+logger = logging.getLogger(__name__)
+with open('/tmp/pod.yaml') as f:
+    dataMap = yaml.safe_load(f)
+    for x in dataMap:
+        for y in dataMap[x]:
+            if (y['role']=='Controller') or (y['role']=='Compute'):
+                ip = str(y['ip'])
+                user = str(y['user'])
+                pwd = str(y['password'])
+                ssh_d = ssh.SSH(user, host= ip, password= pwd)
+                status, stdout, stderr = ssh_d.execute("cd /etc && mkdir barometer-config")
+                if status:
+                    raise Exception("Command failed with non-zero status.")
+                    logger.info(stdout.splitlines())
+                with open("/home/opnfv/bottlenecks/monitor/barometer_client.conf") as stdin_file:
+                    ssh_d.run("cat > /etc/barometer-config/barometer_client_collectd.conf", stdin=stdin_file)
+
+                with open("/home/opnfv/bottlenecks/monitor/barometer_install_client.sh") as stdin_file:
+                    ssh_d.run("cat > /etc/barometer-config/install.sh", stdin=stdin_file)
+                    
+                ssh_d.run("cd /etc/barometer-config/ && bash ./install.sh")
---
+++ monitor/barometer_install_client.sh
@@ -0,0 +1,4 @@
+#docker pull opnfv/barometer
+sudo docker run  --name bottlenecks-barometer-client -tid --net=host -v `pwd`/../src/collectd_sample_configs:/opt/collectd/etc/collectd.conf.d \
+-v /etc/barometer-config/barometer_client_collectd.conf:/src/barometer/src/collectd/collectd/src/collectd.conf \
+-v /var/run:/var/run -v /tmp:/tmp --privileged opnfv/barometer /run_collectd.sh
