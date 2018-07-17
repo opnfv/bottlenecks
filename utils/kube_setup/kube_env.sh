b54d27834bc6204561451acacfd446385c29e6b7
Yang (Gabriel) Yu <Gabriel.yuyang@huawei.com>
2018-07-17 16:15:29 +0800

    Add golang installation
    Bottlenecks container does not have golang installed.
    This patch is to add golang installation script for later
    usage.
    
    Change-Id: I1456bae1e0f0ab018014d0dc9ae6df334123bc9f
    Signed-off-by: Yang (Gabriel) Yu <Gabriel.yuyang@huawei.com>

---
+++ utils/kube_setup/kube_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2018 Huawei Technologies Co.,Ltd and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+export GOROOT=/usr/local/go
+export GOPATH=${HOME}/go
+export PATH=${PATH}:${GOROOT}/bin:${GOPATH}/bin
+
