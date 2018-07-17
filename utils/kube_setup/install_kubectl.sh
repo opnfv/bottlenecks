28fe957cf4089fb57771ef97a2fc92dd27cae81d
Yang (Gabriel) Yu <Gabriel.yuyang@huawei.com>
2018-07-17 16:12:37 +0800

    Add kubectl installation for jump server
    Bottlenecks container does not support kubectl.
    This patch is to add installation script.
    
    Change-Id: I4e066aaffd0dda76b91b71a35e515a78acd5d823
    Signed-off-by: Yang (Gabriel) Yu <Gabriel.yuyang@huawei.com>

---
+++ utils/kube_setup/install_kubectl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2018 Huawei Tech and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+OS_TYPE=$(uname -a)
+OS_UBUNTU=$(echo $OS_TYPE | grep ubuntu)
+
+if [[ $OS_UBUNTU ]]; then
+    sudo apt-get update && sudo apt-get install -y apt-transport-https
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    sudo touch /etc/apt/sources.list.d/kubernetes.list
+    echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+    sudo apt-get update
+    sudo apt-get install -y kubectl
+else
+    cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+    yum install -y kubectl
+fi
+
