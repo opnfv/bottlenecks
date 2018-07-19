0940d845422617167fa3218f56fa77ccd152b651
LeoQi <QibinZheng2014@tongji.edu.cn>
2018-07-20 02:52:12 +0800

    add testsuite and testcase list pages in frontend of testing-scheduler
    JIRA: BOTTLENECK-236
    
    add two base pages: testsuite page and testcase list page.
    
    Change-Id: I2c8eefac58893b0a3c0fb9c20f724f8612893dc0
    Signed-off-by: Zheng Qibin <QibinZheng2014@tongji.edu.cn>

---
+++ testing-scheduler/ui/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import testsuite from '@/components/testsuite'
+import testcase from '@/components/testcase'
+import testcase_content from '@/components/testcase_content'
+import environment from '@/components/environment'
+Vue.use(Router)
+const Result = {template: "<div>please visit <a target='blank' href='http://lab205.jios.org:30002/dashboard/db/cluster?orgId=1'>grafana page.</a></div>"}
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'testsuite',
+      component: testsuite
+    },
+    {
+      path: '/stories',
+      name: 'testcase',
+      component: testcase
+    },
+    {
+      path: '/content',
+      name: 'testcase_content',
+      component: testcase_content
+    },
+    {
+      path: '/result',
+      component: Result
+    },
+    {
+      path: '/report',
+      component: testcase
+    },
+    {
+      path: '/environment',
+      component: environment
+    }
+  ]
+})
