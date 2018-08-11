e811f9e538dc1b5f9a86404252787c6a37674990
Yang (Gabriel) Yu <Gabriel.yuyang@huawei.com>
2018-08-11 11:14:43 +0800

    add k8s util functions for compatibility of upstream changes
    JIRA: BOTTLENECK-243
    
    Change-Id: Idcbe75a4bcd8fedde4e4b95ae999eebbaaa01645
    Signed-off-by: Yang (Gabriel) Yu <Gabriel.yuyang@huawei.com>

--- utils/k8s_setup/k8s_utils.py
+++ utils/k8s_setup/k8s_utils.py
@@ -10,6 +10,8 @@
 
 import os
 import utils.logger as log
+from kubernetes import client, watch
+
 
 LOG = log.Logger(__name__).getLogger()
 INSTALLER_TYPE = os.getenv("INSTALLER_TYPE")
@@ -26,3 +28,23 @@ def get_config_path(INSTALLER_TYPE=None, CONFIG_PATH="/tmp/k8s_config"):
             raise Exception("Must at least specify the path \
 of k8s config!")
     return CONFIG_PATH
+
+
+def get_core_api(version='v1'):
+    if version.lower() == 'v1':
+        API = client.CoreV1Api()
+        LOG.info(API)
+    else:
+        raise Exception("Must input a validate verison!")
+    return API
+
+
+def watch_namespace(namespace='kube-system', count=2, _request_timeout=60):
+    w = watch.Watch()
+    for event in w.stream(namespace, _request_timeout):
+        LOG.info("Event: %s %s" %
+                 (event['type'], event['object'].metadata.name))
+        count -= 1
+        if not count:
+            w.stop()
+    LOG.info("Ended.")
