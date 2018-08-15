108ee8cf93e66710309dd187e6e84334ebdde735
Yang (Gabriel) Yu <Gabriel.yuyang@huawei.com>
2018-08-15 12:39:33 +0800

    add k8s capacity test case
    JIRA: BOTTLENECK-243
    
    Change-Id: I0f36aac10cf0e05560051c785ada397e0c97e112
    Signed-off-by: Yang (Gabriel) Yu <Gabriel.yuyang@huawei.com>

---
+++ testsuites/kubestone/stress_test.py
@@ -0,0 +1,15 @@
+from kubernetes import client, config
+import k8s_utils
+import utils.logger as log
+import yaml
+
+LOG = log.Logger(__name__).getLogger()
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
+
