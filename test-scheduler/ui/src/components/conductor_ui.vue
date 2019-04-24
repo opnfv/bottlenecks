de5d561dcf0791b120087ffb9f7506f41c2cb0e0
tjuyinkanglin <14_ykl@tongji.edu.cn>
2019-04-24 15:09:06 +0800

    Add conductor UI to the result page of test scheduler
    Conductor UI is added to the result page of test scheduler so that users can see execution results of test cases.
    
    Change-Id: I9d4f35bac88be17be4fadf2830ce5073bd9d3818
    Signed-off-by: tjuyinkanglin <14_ykl@tongji.edu.cn>

---
+++ test-scheduler/ui/src/components/conductor_ui.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <iframe src='http://0.0.0.0:5000/#/workflow?q=&h=&workflowTypes=&status=&start=0' id="conductor-ui"></iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    mounted () {
+      /**
+       * iframe adaptive display
+       */
+      function changeConductorUIfIframe () {
+        const conductorUI = document.getElementById('conductor-ui')
+        const deviceWidth = document.body.clientWidth
+        const deviceHeight = document.body.clientHeight
+        conductorUI.style.width = Number(deviceWidth) + 'px'
+        conductorUI.style.height = Number(deviceHeight) + 'px'
+      }
+
+      changeConductorUIfIframe()
+
+      window.onresize = function () {
+        changeConductorUIfIframe()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
